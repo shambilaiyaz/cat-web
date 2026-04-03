04e7fb966c6bbac6beac4a57064a7d9f8fcf8bcc
Shambil Aiyaz <shambilaiyaz@proton.me>
2026-04-03 19:10:09 +0600

    video placed

--- ct.js
+++ ct.js
@@ -2,6 +2,6 @@ function showAlert() {
   alert("welcome to ct web");
 }
 function playSound() {
-  let screenreader = new Audio("/home/sh/Videos/2026-03-31 21-51-52.mkv");
+  let screenreader = new Audio("ct-video/cat-screenreader.mkv");
   screenreader.play();
 }
